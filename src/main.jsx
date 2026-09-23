191635c75e9cc4aafd05277e8ed8da3b2d10186b
Cristiandis <112336919+Cristiandis@users.noreply.github.com>
2026-09-23 21:17:40 +0200

    use HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import App from "./App.jsx";
 import Form from "./Form.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/form" element={<Form />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
