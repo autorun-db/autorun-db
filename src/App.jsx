59a3ad2b45c78ff5ea79875bd16219fcaf2a1154
Cristiandis <izzocristian03@gmail.com>
2026-09-24 09:20:16 +0200

    use an actual grid for game view

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import JSON from "./assets/games.json";
 import { Game } from "./Game";
 import "./common.css";
+import "./App.css";
 
 export default function App() {
   return (
-    <>
+    <div className="game_container">
       {JSON.map((gioco) => {
         return <Game gioco={gioco} key={gioco.name}></Game>;
       })}
-    </>
+    </div>
   );
 }
